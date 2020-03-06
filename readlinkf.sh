3b049aa04bd6f676e5ce8a34dcaefd43774ae0fb
Koichi Nakashima <koichi@nksm.name>
2020-03-06 20:06:58 +0900

    Fix minor bug on bash and other shell

--- readlinkf.sh
+++ readlinkf.sh
@@ -1,27 +1,23 @@
 #shellcheck shell=sh
 
-# readlink (without -f option) implementation
-readlinkf() {
-  p=$1 i=0
-  [ -L "${p%/}" ] && [ ! -e "${p%/}" ] && p=${p%/}
-  while [ $i -lt 10 ]; do
-    case $p in (*/*) cd -P "${p%/*}" 2>/dev/null || return 1; p=${p##*/}; esac
+# readlink without -f option
+readlinkf_readlink() {
+  p=$1 && i=0 && [ -L "${p%/}" ] && [ ! -e "${p%/}" ] && p=${p%/}
+  while [ $i -lt 10 ] && i=$((i+1)); do # maximum recursion depth
+    set -- "$p" "${p%/*}" "${p##*/}" /dev/null
+    [ "$1" = "$2" ] || { [ ! "$2" ] || cd -P "$2" 2>"$4" || return 1; p=$3; }
     [ ! -L "$p" ] && p=${PWD%/}${p:+/}$p && printf '%s\n' "${p:-/}" && return 0
-    p=$(readlink "$p" 2>/dev/null) || return 1
-    i=$((i+1))
-  done
-  return 1
+    p=$(readlink "$p" 2>"$4") || return 1
+  done; return 1
 }
 
-# POSIX compliant implementation
+# POSIX compliant
 readlinkf_posix() {
-  p=$1 i=0
-  [ -L "${p%/}" ] && [ ! -e "${p%/}" ] && p=${p%/}
-  while [ $i -lt 10 ]; do
-    case $p in (*/*) cd -P "${p%/*}" 2>/dev/null || return 1; p=${p##*/}; esac
+  p=$1 && i=0 && [ -L "${p%/}" ] && [ ! -e "${p%/}" ] && p=${p%/}
+  while [ $i -lt 10 ] && i=$((i+1)); do # maximum recursion depth
+    set -- "$p" "${p%/*}" "${p##*/}" /dev/null
+    [ "$1" = "$2" ] || { [ ! "$2" ] || cd -P "$2" 2>"$4" || return 1; p=$3; }
     [ ! -L "$p" ] && p=${PWD%/}${p:+/}$p && printf '%s\n' "${p:-/}" && return 0
-    l=$(ls -dl "$p" 2>/dev/null) || return 1
-    p=${l#*" $p -> "} i=$((i+1))
-  done
-  return 1
+    l=$(ls -dl "$p" 2>"$4") && p=${l#*" $p -> "} || return 1
+  done; return 1
 }
