20633899cf65f7c3160356ae296219243459949e
Koichi Nakashima <koichi@nksm.name>
2020-03-06 22:36:22 +0900

    Make shortern

--- readlinkf.sh
+++ readlinkf.sh
@@ -1,23 +1,21 @@
-#shellcheck shell=sh
+# shellcheck shell=sh
 
 # readlink without -f option
 readlinkf_readlink() {
   p=$1 && i=0 && [ -L "${p%/}" ] && [ ! -e "${p%/}" ] && p=${p%/}
-  while [ $i -lt 10 ] && i=$((i+1)); do # maximum recursion depth
-    set -- "$p" "${p%/*}" "${p##*/}" /dev/null
-    [ "$1" = "$2" ] || { [ ! "$2" ] || cd -P "$2" 2>"$4" || return 1; p=$3; }
+  while [ $i -lt 10 ] && i=$((i+1)); do set -- "${p%/*}" "${p##*/}" /dev/null
+    [ "$p" = "$1" ] || { [ ! "$1" ] || cd -P "$1" 2>"$3" || return 1; p=$2; }
     [ ! -L "$p" ] && p=${PWD%/}${p:+/}$p && printf '%s\n' "${p:-/}" && return 0
-    p=$(readlink "$p" 2>"$4") || return 1
-  done; return 1
+    p=$(readlink "$p" 2>"$3") || return 1
+  done; return 1 # maximum recursive loop exceeded
 }
 
 # POSIX compliant
 readlinkf_posix() {
   p=$1 && i=0 && [ -L "${p%/}" ] && [ ! -e "${p%/}" ] && p=${p%/}
-  while [ $i -lt 10 ] && i=$((i+1)); do # maximum recursion depth
-    set -- "$p" "${p%/*}" "${p##*/}" /dev/null
-    [ "$1" = "$2" ] || { [ ! "$2" ] || cd -P "$2" 2>"$4" || return 1; p=$3; }
+  while [ $i -lt 10 ] && i=$((i+1)); do set -- "${p%/*}" "${p##*/}" /dev/null
+    [ "$p" = "$1" ] || { [ ! "$1" ] || cd -P "$1" 2>"$3" || return 1; p=$2; }
     [ ! -L "$p" ] && p=${PWD%/}${p:+/}$p && printf '%s\n' "${p:-/}" && return 0
-    l=$(ls -dl "$p" 2>"$4") && p=${l#*" $p -> "} || return 1
-  done; return 1
+    l=$(ls -dl "$p" 2>"$3") && p=${l#*" $p -> "} || return 1
+  done; return 1 # maximum recursive loop exceeded
 }
