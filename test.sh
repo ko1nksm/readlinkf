3323eb90b35f9277bf7510b39b564a577a56abfe
Koichi Nakashima <koichi@nksm.name>
2020-03-07 14:42:48 +0900

    Refactor tests

--- test.sh
+++ test.sh
@@ -14,10 +14,11 @@ else
   if [ ! -e /.dockerenv ]; then
     docker --version >&2 || abort "[ERROR] You need docker to run"
     set -- "${1:-sh}" "${2:-Dockerfile}" "${3:-latest}"
-    docker run --rm "$(docker build --build-arg "TAG=$3" -q . -f "$2")" "$1" "./${0##*/}"
+    docker run --rm -t "$(docker build --build-arg "TAG=$3" -q . -f "$2")" "$1" "./${0##*/}"
     exit
   fi
 fi
+
 
 echo "============================ /etc/os-release ==========================="
 [ -f /etc/os-release ] && cat /etc/os-release
@@ -43,11 +44,49 @@ make_file "/RLF-SPACE INCLUDED/FILE NAME"
 make_link "/RLF-SPACE INCLUDED/DIR NAME/SYMBOLIC LINK -> ../FILE NAME"
 
 echo "--------------------------------- Tree ---------------------------------"
-run tree -N --noreport -I "*[a-z]*" /
+run tree -C -N --noreport -I "*[a-z]*" /
 
 echo "--------------------------------- Tests --------------------------------"
 TEST_COUNT=$((44 * 3))
-compare() {
+# TEST_COUNT=$((1 * 3))
+
+pathes() {
+  # echo "/RLF-BASE/FILE"; return # if you want to run only one path.
+  {
+    find /RLF-*
+    echo "/RLF-BASE/LINK2/FILE"
+  } | sort | while IFS= read -r pathname; do
+    echo "$pathname"
+    echo "$pathname/"
+  done
+}
+
+tests() {
+  ex=0 count=0
+  while IFS= read -r pathname; do
+    set -- "$pathname"
+    cwd=$PWD
+
+    # absolute path
+    count=$((count+1)) && compare_with_readlink "$pathname" || ex=1
+
+    cd / # relative path from current directory
+    count=$((count+1)) && compare_with_readlink "${pathname#/}" || ex=1
+
+    cd /usr/bin # relative path from other directory
+    count=$((count+1)) && compare_with_readlink "../..$pathname" || ex=1
+
+    cd "$cwd"
+  done
+  if [ "$count" -ne "$TEST_COUNT" ]; then
+    set -- "$TEST_COUNT" "$count"
+    printf '\033[31m[fail]\033[m test count: expected %d, but ran %d\n' "$@"
+    ex=1
+  fi
+  return "$ex"
+}
+
+compare_with_readlink() {
   # shellcheck disable=SC2230
   link=$($(which readlink) -f "$1") &&:; set -- "$@" "$link" "$?"
   link=$(readlinkf_readlink "$1") &&:; set -- "$@" "$link" "$?"
@@ -61,45 +100,18 @@ compare() {
   fi
 }
 
-{
-  find /RLF-*
-  echo "/RLF-BASE/LINK2/FILE"
-} | sort | while IFS= read -r pathname; do
-  echo "$pathname"
-  echo "$pathname/"
-done | {
-  ex=0 count=0
-  while IFS= read -r pathname; do
-    set -- "$pathname"
-    cwd=$PWD
+pathes | tests &&:
+ex=$?
 
-    # absolute path
-    count=$((count+1)) && compare "$pathname" || ex=1
+echo "-------------------------------- Cleanup -------------------------------"
+run rm -rf "/RLF-BASE"
+run rm -rf "/RLF-BASE1"
+run rm -rf "/RLF-LINK4"
+run rm -rf "/RLF-LOOP1"
+run rm -rf "/RLF-LOOP2"
+run rm -rf "/RLF-MISSING"
+run rm -rf "/RLF-ROOT"
+run rm -rf "/RLF-SPACE INCLUDED"
+run tree -C -N --noreport -I "*[a-z]*" /
 
-    cd / # relative path from current directory
-    count=$((count+1)) && compare "${pathname#/}" || ex=1
-
-    cd /usr/bin # relative path from other directory
-    count=$((count+1)) && compare "../..$pathname" || ex=1
-
-    cd "$cwd"
-  done
-  if [ "$count" -ne "$TEST_COUNT" ]; then
-    set -- "$TEST_COUNT" "$count"
-    printf '\033[31m[fail]\033[m test count: expected %d, but ran %d\n' "$@"
-    ex=1
-  fi
-
-  echo "-------------------------------- Cleanup -------------------------------"
-  run rm -rf "/RLF-BASE"
-  run rm -rf "/RLF-BASE1"
-  run rm -rf "/RLF-LINK4"
-  run rm -rf "/RLF-LOOP1"
-  run rm -rf "/RLF-LOOP2"
-  run rm -rf "/RLF-MISSING"
-  run rm -rf "/RLF-ROOT"
-  run rm -rf "/RLF-SPACE INCLUDED"
-  run tree -N --noreport -I "*[a-z]*" /
-
-  exit $ex
-}
+exit $ex
