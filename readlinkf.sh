48c1c259ae9a0adbffc154731a3b1a431843e191
Koichi Nakashima <koichi@nksm.name>
2020-05-14 22:37:11 +0900

    Refactor: Merge private function

--- readlinkf.sh
+++ readlinkf.sh
@@ -30,71 +30,71 @@
 
 # readlink without -f option
 readlinkf_readlink() {
-  eval "$1=\$(readlinkf_readlink_ \"\$2\")"
-}
+  if [ $# -gt 1 ]; then
+    eval "$1=\$(readlinkf_readlink \"\$2\")"
+  else
+    [ ${1:+x} ] || return 1
+    target=$1 loop=10 CDPATH=""
 
-readlinkf_readlink_() {
-  [ ${1:+x} ] || return 1
-  target=$1 loop=10 CDPATH=""
+    if [ ! -e "${target%/}" ]; then
+      while [ ! "${target%/}" = "$target" ]; do
+        target=${target%/}
+      done
+    fi
+    [ -d "${target:-/}" ] && target="$target/"
 
-  if [ ! -e "${target%/}" ]; then
-    while [ ! "${target%/}" = "$target" ]; do
-      target=${target%/}
+    cd -P "$PWD" 2>/dev/null || return 1
+    while [ "$loop" -gt 0 ] && loop=$(($loop - 1)); do
+      if [ ! "$target" = "${target%/*}" ]; then
+        cd -P "${target%/*}/" 2>/dev/null || break
+        target=${target##*/}
+      fi
+
+      if [ ! -L "$target" ]; then
+        target="${PWD%/}${target:+/}$target"
+        printf '%s\n' "${target:-/}"
+        return 0
+      fi
+
+      target=$(readlink "$target" 2>/dev/null) || break
     done
+    return 1
   fi
-  [ -d "${target:-/}" ] && target="$target/"
-
-  cd -P "$PWD" 2>/dev/null || return 1
-  while [ "$loop" -gt 0 ] && loop=$(($loop - 1)); do
-    if [ ! "$target" = "${target%/*}" ]; then
-      cd -P "${target%/*}/" 2>/dev/null || break
-      target=${target##*/}
-    fi
-
-    if [ ! -L "$target" ]; then
-      target="${PWD%/}${target:+/}$target"
-      printf '%s\n' "${target:-/}"
-      return 0
-    fi
-
-    target=$(readlink "$target" 2>/dev/null) || break
-  done
-  return 1
 }
 
 # POSIX compliant
 readlinkf_posix() {
-  eval "$1=\$(readlinkf_posix_ \"\$2\")"
-}
+  if [ $# -gt 1 ]; then
+    eval "$1=\$(readlinkf_posix \"\$2\")"
+  else
+    [ ${1:+x} ] || return 1
+    target=$1 loop=10 CDPATH=""
 
-readlinkf_posix_() {
-  [ ${1:+x} ] || return 1
-  target=$1 loop=10 CDPATH=""
+    if [ ! -e "${target%/}" ]; then
+      while [ ! "${target%/}" = "$target" ]; do
+        target=${target%/}
+      done
+    fi
+    [ -d "${target:-/}" ] && target="$target/"
 
-  if [ ! -e "${target%/}" ]; then
-    while [ ! "${target%/}" = "$target" ]; do
-      target=${target%/}
+    cd -P "$PWD" 2>/dev/null || return 1
+    while [ "$loop" -gt 0 ] && loop=$(($loop - 1)); do
+      if [ ! "$target" = "${target%/*}" ]; then
+        cd -P "${target%/*}/" 2>/dev/null || break
+        target=${target##*/}
+      fi
+
+      if [ ! -L "$target" ]; then
+        target="${PWD%/}${target:+/}$target"
+        printf '%s\n' "${target:-/}"
+        return 0
+      fi
+
+      link=$(ls -dl "$target" 2>/dev/null) || break
+      target=${link#*" $target -> "}
     done
+    return 1
   fi
-  [ -d "${target:-/}" ] && target="$target/"
-
-  cd -P "$PWD" 2>/dev/null || return 1
-  while [ "$loop" -gt 0 ] && loop=$(($loop - 1)); do
-    if [ ! "$target" = "${target%/*}" ]; then
-      cd -P "${target%/*}/" 2>/dev/null || break
-      target=${target##*/}
-    fi
-
-    if [ ! -L "$target" ]; then
-      target="${PWD%/}${target:+/}$target"
-      printf '%s\n' "${target:-/}"
-      return 0
-    fi
-
-    link=$(ls -dl "$target" 2>/dev/null) || break
-    target=${link#*" $target -> "}
-  done
-  return 1
 }
 
 # The format of "ls -dl" of symlink is defined below
